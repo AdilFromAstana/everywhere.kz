f624462c5f043f511983e70cf8197b83a358fd7f
Rahat Zhanshugurov <zhanshugurov.dev@gmail.com>
2024-01-03 14:07:38 +0600

    Widget initalize new version

---
+++ src/components/KazticketButton.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import React from 'react';
+
+interface ButtonProps {
+    eventId: string;
+    eventCode: string;
+    locale: any;
+}
+
+const KazticketButton = ({ eventId, eventCode, locale }: ButtonProps) => {
+    const buyTicketFromKazticket = () => {
+        if (eventId && eventCode) {
+            //@ts-expect-error: Frontend
+            kztw.create(eventId, eventCode);
+        }
+    };
+    return (
+        <button
+            onClick={() => buyTicketFromKazticket()}
+            className="kazticket-buy-button z-0 cursor-pointer bg-sky-500 lg:w-56 w-full px-2 py-2 lg:px-6 lg:py-4 rounded-xl text-base lg:text-xl font-bold text-white transition duration-500 hover:shadow-2xl"
+        >
+            {locale.EventPage.BuyTicket}
+        </button>
+    );
+};
+
+export default KazticketButton;
