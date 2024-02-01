202eafd755099d4772372f396b8ece5b2657895e
Rahat Zhanshugurov <zhanshugurov.dev@gmail.com>
2024-02-01 22:32:09 +0600

    fix urls

--- src/app/selections/[code]/page.tsx
+++ src/app/selections/[code]/page.tsx
@@ -90,9 +90,6 @@ async function GetEvents(selectionId: string) {
 
         const data = await res.json();
 
-        // const groupedServices = groupArray(!isEmpty(data.serviceGroups) ? data.serviceGroups : [], 'serviceGroupName');
-
-        console.log('data: ', data);
         return data;
     }
     return [];
@@ -145,7 +142,7 @@ export default async function EventSelectionPage({ params }: Props) {
                 <nav className="flex my-2" aria-label="Breadcrumb">
                     <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
                         <li className="inline-flex items-center">
-                            <a
+                            <Link
                                 href="/"
                                 className="inline-flex items-center text-lg font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
                             >
@@ -159,7 +156,7 @@ export default async function EventSelectionPage({ params }: Props) {
                                     <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
                                 </svg>
                                 {locale.SelectionsPage.MainPage}
-                            </a>
+                            </Link>
                         </li>
                         <li>
                             <div className="flex items-center">
@@ -178,12 +175,9 @@ export default async function EventSelectionPage({ params }: Props) {
                                         d="m1 9 4-4-4-4"
                                     />
                                 </svg>
-                                <a
-                                    href="#"
-                                    className="ms-1 text-lg font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white"
-                                >
+                                <span className="ms-1 text-lg font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">
                                     {locale.SelectionsPage.SelectionOfEvents}
-                                </a>
+                                </span>
                             </div>
                         </li>
                         <li aria-current="page">
