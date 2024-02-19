0393e2bdc4a44ad9a6b062cacf60912bd2457e22
Rakhat Zhanshugurov <zhanshugurov.dev@gmail.com>
2024-02-20 00:29:42 +0700

    kostyl 2

--- src/components/EventDateInfo.tsx
+++ src/components/EventDateInfo.tsx
@@ -10,27 +10,47 @@ import { useEffect, useState } from 'react';
 
 interface EventDateProps {
     date: any;
+    isKostyl?: boolean;
 }
 
-const EventDateInfo = ({ date }: EventDateProps) => {
+const EventDateInfo = ({ date, isKostyl }: EventDateProps) => {
     const [data, setData] = useState('');
     const UserLang = getCookie('UserLang');
 
     useEffect(() => {
-        if (!UserLang) {
-            setData(moment(date).locale('ru_Ru').format('Do MMMM HH:mm'));
+        if (isKostyl) {
+            if (!UserLang) {
+                setData(moment(date).utc().add(6, 'h').locale('ru_Ru').format('Do MMMM HH:mm'));
+            } else {
+                switch (UserLang.toLocaleLowerCase()) {
+                    case 'kz':
+                        setData(`${moment(date).utc().add(6, 'h').locale('kz').format('Do MMMM HH:mm')}`);
+                        break;
+                    case 'en':
+                        setData(`${moment(date).utc().add(6, 'h').locale('en').format('Do MMMM HH:mm')}`);
+                        break;
+                    case 'ru':
+                    default:
+                        setData(`${moment(date).utc().add(6, 'h').locale('ru_Ru').format('Do MMMM HH:mm')}`);
+                        break;
+                }
+            }
         } else {
-            switch (UserLang.toLocaleLowerCase()) {
-                case 'kz':
-                    setData(`${moment(date).locale('kz').format('Do MMMM HH:mm')}`);
-                    break;
-                case 'en':
-                    setData(`${moment(date).locale('en').format('Do MMMM HH:mm')}`);
-                    break;
-                case 'ru':
-                default:
-                    setData(`${moment(date).locale('ru_Ru').format('Do MMMM HH:mm')}`);
-                    break;
+            if (!UserLang) {
+                setData(moment(date).locale('ru_Ru').format('Do MMMM HH:mm'));
+            } else {
+                switch (UserLang.toLocaleLowerCase()) {
+                    case 'kz':
+                        setData(`${moment(date).locale('kz').format('Do MMMM HH:mm')}`);
+                        break;
+                    case 'en':
+                        setData(`${moment(date).locale('en').format('Do MMMM HH:mm')}`);
+                        break;
+                    case 'ru':
+                    default:
+                        setData(`${moment(date).locale('ru_Ru').format('Do MMMM HH:mm')}`);
+                        break;
+                }
             }
         }
     }, []);
