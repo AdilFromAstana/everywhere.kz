cbc76fa3ce0f45f0295e1294e8ef93a7dcb3e63d
Adil Aizhanov <adilfirstus@gmail.com>
2024-02-22 11:55:54 +0600

    add new card to contacts page

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -13,8 +13,8 @@ export default async function ContactsPage() {
             <h2 className="mb-5 text-5xl md:text-6xl xl:text-6xl text-center font-bold font-heading tracking-px-n leading-none">
                 {locale.Contacts.ContactsAndDetails}
             </h2>
-            <div className="flex flex-wrap -m-3">
-                <div className="w-full md:w-1/3 p-3">
+            <div className="flex flex-wrap xl:flex xl:flex-wrap md:grid md:grid-cols-2 -m-3">
+                <div className="w-full xl:w-1/4 p-3">
                     <div className="p-11 h-full text-center bg-white bg-opacity-90 border border-blueGray-100 rounded-xl shadow-11xl">
                         <div className="mb-6 relative mx-auto w-16 h-16 bg-white border border-blueGray-200 rounded-full flex items-center justify-center">
                             <svg
@@ -48,7 +48,7 @@ export default async function ContactsPage() {
                         </Link>
                     </div>
                 </div>
-                <div className="w-full md:w-1/3 p-3">
+                <div className="w-full xl:w-1/4 p-3">
                     <div className="p-11 h-full text-center bg-white bg-opacity-90 border border-blueGray-100 rounded-xl shadow-11xl">
                         <div className="mb-6 relative mx-auto w-16 h-16 bg-white border border-blueGray-200 rounded-full flex items-center justify-center">
                             <svg
@@ -82,7 +82,43 @@ export default async function ContactsPage() {
                         </Link>
                     </div>
                 </div>
-                <div className="w-full md:w-1/3 p-3">
+                <div className="w-full xl:w-1/4 p-3">
+                    <div className="p-11 h-full text-center bg-white bg-opacity-90 border border-blueGray-100 rounded-xl shadow-11xl">
+                        <div className="mb-6 relative mx-auto w-16 h-16 bg-white border border-blueGray-200 rounded-full flex items-center justify-center">
+                            <svg
+                                width="32"
+                                height="33"
+                                viewBox="0 0 32 33"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M4 7.16667C4 5.69391 5.19391 4.5 6.66667 4.5H11.039C11.6129 4.5 12.1224 4.86724 12.3039 5.4117L14.301 11.4029C14.5108 12.0324 14.2258 12.7204 13.6324 13.0172L10.6227 14.522C12.0923 17.7816 14.7184 20.4077 17.978 21.8773L19.4828 18.8676C19.7796 18.2742 20.4676 17.9892 21.0971 18.199L27.0883 20.1961C27.6328 20.3776 28 20.8871 28 21.461V25.8333C28 27.3061 26.8061 28.5 25.3333 28.5H24C12.9543 28.5 4 19.5457 4 8.5V7.16667Z"
+                                    stroke="#4F46E5"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                />
+                            </svg>
+                        </div>
+                        <h3 className="mb-3 text-xl font-bold font-heading leading-snug">
+                            {locale.Contacts.PurchaseAndReturnForAlau}
+                        </h3>
+                        <p className="font-medium text-gray-600">
+                            {locale.Contacts.ForQuestionsRegardingThePurchaseForAlau}
+                        </p>
+                        <p className="font-medium leading-relaxed">+7-771-247-67-48</p>
+                        <Link className="my-2" href="tel:+77712476748">
+                            <div className="text-white mt-2 rounded-md bg-sky-500 px-3 py-2 ">
+                                {locale.Contacts.Call}
+                            </div>
+                        </Link>
+                        <Link className="my-2" target="_blank" href="https://wa.me/77712476748">
+                            <div className="text-white mt-2 rounded-md bg-emerald-500 px-3 py-2 ">WhatsApp</div>
+                        </Link>
+                    </div>
+                </div>
+                <div className="w-full xl:w-1/4 p-3">
                     <div className="p-11 h-full text-center bg-white bg-opacity-90 border border-blueGray-100 rounded-xl shadow-11xl">
                         <div className="mb-6 relative mx-auto w-16 h-16 bg-white border border-blueGray-200 rounded-full flex items-center justify-center">
                             <svg
