2e7327a1dd659ad1a64739b1278d8d51ae0d1e05
Rahat Zhanshugurov <zhanshugurov.dev@gmail.com>
2024-02-29 01:58:32 +0600

    Fix event poster in event page

--- src/app/event/[code]/page.tsx
+++ src/app/event/[code]/page.tsx
@@ -118,7 +118,7 @@ export default async function EventPage({ params }: Props) {
                         height={432}
                         width={756}
                         src={data.posterFileUrl ?? EmptyPoster.src}
-                        className="lg:h-full h-full lg:w-fit rounded-xl"
+                        className="lg:h-full h-full w-fit rounded-xl"
                     />
                     {data.statusId === EventStatuses.SoldOut && (
                         <Image
